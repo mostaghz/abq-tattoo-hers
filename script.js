169048eb4236e53c2d21e5464029d66a9a403814
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-10-27 17:50:34 -0600

    created artist array (only 2 artists so far)

---
+++ script.js
@@ -0,0 +1,21 @@
+let artists = [
+    // need to figure out how to hyperlink objects
+    // also need to determine if a seconf array needs to be created for subjects, like "Tattooing at: ", "Booking: ", etc.
+    {
+        name: "Chelsey Moore",
+        instagram: "@cmoore04",
+        shop: "Archetype Tattoo Studio",
+        booking: "chelseymooretattoo.com",
+        specialtiesOne: "Color Illustrative",
+        specialtiesTwo: "Color Realism",
+    },
+    {
+        name: "Fox Richards",
+        instagram: "@fox_richards",
+        shop: "Coven Studio",
+        booking: "foxrichards.com",
+        specialtiesOne: "Black and Gray Realism mixed with Geometric and Blackwork Elements",
+    },
+]
+
+console.log(artists)
