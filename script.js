0ff8eac39cd232e86cd34764cda0f6eb19c2b64b
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-10-27 18:00:25 -0600

    added chelsey and fox image files, as well as the img key value pairs

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 let artists = [
     // need to figure out how to hyperlink objects
-    // also need to determine if a seconf array needs to be created for subjects, like "Tattooing at: ", "Booking: ", etc.
+    // also need to determine if a second array needs to be created for subjects, like "Tattooing at: ", "Booking: ", etc.
     {
-        name: "Chelsey Moore",
+        img: 'images/chelsey-moore.jpg',
+        name: 'Chelsey Moore',
         instagram: "@cmoore04",
-        shop: "Archetype Tattoo Studio",
-        booking: "chelseymooretattoo.com",
-        specialtiesOne: "Color Illustrative",
-        specialtiesTwo: "Color Realism",
+        shop: 'Archetype Tattoo Studio',
+        booking: 'chelseymooretattoo.com',
+        specialtiesOne: 'Color Illustrative',
+        specialtiesTwo: 'Color Realism',
     },
     {
-        name: "Fox Richards",
-        instagram: "@fox_richards",
-        shop: "Coven Studio",
-        booking: "foxrichards.com",
-        specialtiesOne: "Black and Gray Realism mixed with Geometric and Blackwork Elements",
+        img: 'images/fox-richards.jpeg',
+        name: 'Fox Richards',
+        instagram: '@fox_richards',
+        shop: 'Coven Studio',
+        booking: 'foxrichards.com',
+        specialtiesOne: 'Black and Gray Realism mixed with Geometric and Blackwork Elements',
     },
 ]
 
