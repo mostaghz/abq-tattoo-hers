d2fe7e43e89d14e66dd51c9d3ab4da25a0a43a99
Alex Mostaghni <113554195+mostaghz@users.noreply.github.com>
2022-10-29 17:31:52 -0600

    figured out how to make my header image the width of the window without a crazy zoom or repeating tiles git push

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// // LIST SECTION TITLES ARRAY
+//
+// let sectionTitles = [
+//     {
+//         instagramTitle: '',
+//         shopTitle: 'Shop:',
+//         bookingTitle: 'Booking:',
+//         specialtiesTitle: 'Specialties:',
+//     }
+// ]
+
 // ARTISTS ARRAY
 
 let artists = [
@@ -7,8 +18,9 @@ let artists = [
         alt: 'Chelsey sitting in her tattooing space',
         name: 'Chelsey Moore',
         instagram: '@cmoore04',
-        shop: '<a href="http://archetypetattoo.com/">Archetype Tattoo Studio</a>',
-        booking: '<a href="https://chelseymooretattoo.wordpress.com/">chelseymooretattoo.com</a>',
+        shop: 'Shop: <a href="http://archetypetattoo.com/">Archetype Tattoo Studio</a>',
+        booking: 'Booking: <a href="https://chelseymooretattoo.wordpress.com/">chelseymooretattoo.com</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: 'Color Illustrative',
         specialtiesTwo: 'Color Realism',
         specialtiesThree: '',
@@ -19,8 +31,9 @@ let artists = [
         alt: 'rainbow logo of a fox head',
         name: 'Fox Richards',
         instagram: '@fox_richards',
-        shop: 'Coven Studio',
-        booking: '<a href="https://www.foxrichards.com/">foxrichards.com</a>',
+        shop: 'Shop: Coven Studio',
+        booking: 'Booking: <a href="https://www.foxrichards.com/">foxrichards.com</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: 'Black and Gray Realism mixed with Geometric and Blackwork',
         specialtiesTwo: '',
         specialtiesThree: '',
@@ -30,8 +43,9 @@ let artists = [
         img: '',
         name: 'Arianna Shelton',
         instagram: '@weirdmullet',
-        shop: '<a href="https://www.milagrotattoo.com/">Milagro Tattoo</a>',
-        booking: '<a href="https://www.milagrotattoo.com/ariannashelton">milagrotattoo.com/ariannashelton</a>',
+        shop: 'Shop: <a href="https://www.milagrotattoo.com/">Milagro Tattoo</a>',
+        booking: 'Booking: <a href="https://www.milagrotattoo.com/ariannashelton">milagrotattoo.com/ariannashelton</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: '',
         specialtiesTwo: '',
         specialtiesThree: '',
@@ -41,8 +55,9 @@ let artists = [
         img: '',
         name: 'Maggie Elena',
         instagram: '@maggie.elena',
-        shop: '<a href="https://blackalchemystudio.com/">Black Alchemy</a>',
-        booking: '<a href="mailto:maggieelenatattoo@gmail.com">maggieelenatattoo@gmail.com</a>',
+        shop: 'Shop: <a href="https://blackalchemystudio.com/">Black Alchemy</a>',
+        booking: 'Booking: <a href="mailto:maggieelenatattoo@gmail.com">maggieelenatattoo@gmail.com</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: 'Heavy Blackwork',
         specialtiesTwo: 'Neo-Tribal',
         specialtiesThree: '',
@@ -52,8 +67,9 @@ let artists = [
         img: '',
         name: 'Gina Marie Medlock',
         instagram: '@ginamarietattoo',
-        shop: '<a href="https://www.highhandstattoo.com/">High Hands Tattoo</a>',
-        booking: '<a href="https://www.ginamarietattoo.com/">ginamarietattoo.com</a>',
+        shop: 'Shop: <a href="https://www.highhandstattoo.com/">High Hands Tattoo</a>',
+        booking: 'Booking: <a href="https://www.ginamarietattoo.com/">ginamarietattoo.com</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: '',
         specialtiesTwo: '',
         specialtiesThree: '',
@@ -63,8 +79,9 @@ let artists = [
         img: '',
         name: 'Kori',
         instagram: '@korixmarie',
-        shop: '<a href="https://www.instagram.com/cellardoorabq/?hl=en">Cellardoor Collective</a>',
-        booking: '<a href="mailto:korimarietattoo@gmail.com">korimarietattoo@gmail.com</a>',
+        shop: 'Shop: <a href="https://www.instagram.com/cellardoorabq/?hl=en">Cellardoor Collective</a>',
+        booking: 'Booking: <a href="mailto:korimarietattoo@gmail.com">korimarietattoo@gmail.com</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: '',
         specialtiesTwo: '',
         specialtiesThree: '',
@@ -74,8 +91,9 @@ let artists = [
         img: '',
         name: 'Isha Luz',
         instagram: 'ishaluz.tattoos',
-        shop: '<a href="https://www.cheekymonkeyabq.com/">Cheeky Monkey Tattoo</a>',
-        booking: '<a href ="form.jotform.com/222216047476050">form.jotform.com/222216047476050</a>',
+        shop: 'Shop: <a href="https://www.cheekymonkeyabq.com/">Cheeky Monkey Tattoo</a>',
+        booking: 'Booking: <a href ="form.jotform.com/222216047476050">form.jotform.com/222216047476050</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: 'Anime',
         specialtiesTwo: 'Fine Line',
         specialtiesThree: 'Illustrative',
@@ -85,8 +103,9 @@ let artists = [
         img: '',
         name: 'Sydney Settecerri',
         instagram: '@squidartificial',
-        shop: '<a href="https://www.cheekymonkeyabq.com/">Cheeky Monkey Tattoo</a>',
-        booking: '<a href="mailto:shop@cheekymonkeyabq.com">shop@cheekymonkeyabq.com</a>',
+        shop: 'Shop: <a href="https://www.cheekymonkeyabq.com/">Cheeky Monkey Tattoo</a>',
+        booking: 'Booking: <a href="mailto:shop@cheekymonkeyabq.com">shop@cheekymonkeyabq.com</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: '*Apprentice',
         specialtiesTwo: '',
         specialtiesThree: '',
@@ -96,8 +115,9 @@ let artists = [
         img: '',
         name: 'Reina Plant',
         instagram: '@reina.plant',
-        shop: '<a href="https://www.allisonetattoo.com/">All is One Tattoo</a>',
-        booking: '<a href="mailto:korimarietattoo@gmail.com">allisonetattoo@gmail.com</a>',
+        shop: 'Shop: <a href="https://www.allisonetattoo.com/">All is One Tattoo</a>',
+        booking: 'Booking: <a href="mailto:korimarietattoo@gmail.com">allisonetattoo@gmail.com</a>',
+        specialtiesTitle: 'Specialties:',
         specialtiesOne: 'American Traditional',
         specialtiesTwo: '',
         specialtiesThree: '',
@@ -116,13 +136,14 @@ function handleLoadEvent () {
             return (
                 `${artistDirectory}
                     <div class="col">
-                        <div class="card">
+                        <div class="card border border-light">
                             <img src="${currentDataElement.img}" alt="${currentDataElement.img}" class="card-img-top">
                             <div class="card-body">
                                 <h5 class="card-title">${currentDataElement.name}</h5>
                                  <p class="card-text">${currentDataElement.instagram}</p>
                                  <p class="card-text">${currentDataElement.shop}</p>
                                  <p class="card-text">${currentDataElement.booking}</p>
+                                 <p class="card-text">${currentDataElement.specialtiesTitle}</p>
                                  <p class="card-text">${currentDataElement.specialtiesOne}</p>
                                  <p class="card-text">${currentDataElement.specialtiesTwo}</p>
                                  <p class="card-text">${currentDataElement.specialtiesThree}</p>
